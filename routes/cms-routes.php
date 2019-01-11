ab85c4c63b225c3d871af75a32f1bd7899bfc2da
Paul Burdick <paul@reedmaniac.com>
2019-01-11 10:49:56 -0800

    More precision
    Just in case anyone wants to do something like “docs-for-my-plugin”

--- routes/cms-routes.php
+++ routes/cms-routes.php
@@ -8,7 +8,8 @@ Route::pattern('any', '.*');
 $router->group(['middleware' => 'web'], function ($router) use ($cp_path) {
     $router->get($cp_path.'/javascript/{any}', 'Cp\Controller@javascript');
     $router->get($cp_path.'/css/{any}', 'Cp\Controller@css');
-    $router->get($cp_path.'/docs{any}', 'Cp\DocsController@show');
+    $router->get($cp_path.'/docs', 'Cp\DocsController@show');
+    $router->get($cp_path.'/docs/{any}', 'Cp\DocsController@show');
     $router->any($cp_path.'{any}', 'Cp\Controller@all');
 
     // This makes sure the Laravel Debugbar routes work
